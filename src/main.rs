54e4ea054c9d21822fe850c6811a97e9ea3a9dfe
raul.albin <raul200794@gmail.com>
2024-12-23 17:15:52 +0100

    Add High order function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use point::Point;
 use square::Square;
-
+use triangle::Triangle;
 mod point;
 mod square;
+mod triangle;
 
 fn main() {
     let test = 5;
@@ -32,5 +33,41 @@ fn main() {
         Point { x: 0, y: 0 },
     );
 
-    println!("My square {:?} has an area of {} and a perimeter of {}",square,square.area(),square.perimeter());
+    println!(
+        "My square {:?} has an area of {} and a perimeter of {}",
+        square,
+        square.area(),
+        square.perimeter()
+    );
+
+    let upper = 200;
+
+    let sum_of_squared_odd_numbers: u32 = (0..)
+        .map(|n| n * n) // All natural numbers squared
+        .take_while(|&n_squared| n_squared < upper) // Below upper limit
+        .filter(|&n_squared| is_odd(n_squared)) // That are odd
+        .sum(); // Sum them
+    println!("functional style: {}", sum_of_squared_odd_numbers);
+
+    let triangle = Triangle::new(
+        Point { x: 0, y: 0 },
+        Point { x: 4, y: 0 },
+        Point { x: 3, y: 5 },
+    );
+    let hipotenusa = triangle.hypotenuse();
+    println!("La hipotenusa es {}", hipotenusa);
+    let ab = triangle.a();
+    let bc = triangle.b();
+    let ca = triangle.c();
+
+    println!("El lado AB es {}, el lado BC es {}, y el lado CA es {}",ab,bc,ca);
+
+    // test high order fn
+    let length = triangle.high_order_function(|l| format!("La longitud es de {}",l));
+    println!("length {}",length);
+    
+}
+
+fn is_odd(n: u32) -> bool {
+    return n % 2 == 0;
 }
---
+++ src/triangle.rs
@@ -0,0 +1,60 @@
+    use crate::point::Point;
+    use rand::{random, Rng};
+
+    pub struct Triangle {
+        a: Point,
+        b: Point,
+        c: Point,
+    }
+
+    impl Triangle {
+        pub fn new(a: Point, b: Point, c: Point) -> Triangle {
+            return Triangle { a, b, c };
+        }
+        // Defined by /AB
+        pub fn a(&self) -> f32 {
+            return self._module(&self.a, &self.b);
+        }
+        // Defined by /BC
+        pub fn b(&self) -> f32 {
+            return self._module(&self.b, &self.c);
+        }
+
+        // Defined by /CA
+        pub fn c(&self) -> f32 {
+            return self._module(&self.c, &self.a);
+        }
+
+        pub fn hypotenuse(&self) -> f32 {
+            let ab = self.a();
+            let bc = self.b();
+            let ca = self.c();
+
+            let mut vector = vec![ab, bc, ca];
+            vector.sort_by(|a, b| b.partial_cmp(a).unwrap_or(std::cmp::Ordering::Equal));
+            return vector[0];
+        }
+
+        fn _module(&self, a: &Point, b: &Point) -> f32 {
+            let Point { x: x1, y: y1 } = a;
+            let Point { x: x2, y: y2 } = b;
+            let a2 = (x1 - x2).abs();
+            let b2 = (y1 - y2).abs();
+
+            return (((b2 * b2) as f32) + ((a2 * a2) as f32)).sqrt();
+        }
+
+        pub fn high_order_function<F: Fn(i32) -> String>(&self, func: F) -> i32{
+            
+            let mut len = 0;
+            let mut rng = rand::thread_rng();
+            let random_number: i32 = rng.gen_range(1..80001);
+
+            len = func(random_number).len();
+
+            println!("El número aleatorio ha sido {}",random_number);
+            
+            return len as i32;
+
+        }
+    }
